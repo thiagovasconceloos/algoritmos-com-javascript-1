7d71958707d9ee97745936692ee4ce1b0e25958c
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-02-06 18:57:24 -0300

    Terceira Aula

--- selectionSort.js
+++ selectionSort.js
@@ -1,14 +1,5 @@
- // o metodo selection sort seleciona a posicao inicial e faz a comparação com os outros elementos do vetor
- // caso o  proximo elemento seja menor, ele muda o valor do menor elemento
- // a partir do index do menor elemento, ele continua a buscar os demais elementos no array 
- // vamos supor temos o vetor  v=[1,2,3]
- // o selection vai olhar o elemento na posicao 0 é menor que o elemento na posicao 1 ?
- // se for, fara a mesma comparação com o elemento na posicao 2 
- // sendo menorAtual = 0 
- // posicao atual = 1 
+ // o metodo selection sort seleciona uma posicao x e faz a comparação com os outros elementos do vetor
 
- 
- 
  const livros = require('./lista-livros');
 const menorValor = require('./menor-valor');
  for (let atual = 0; atual < livros.length;atual ++){
